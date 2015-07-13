caee8fe8b3c330f2d809cfb30e0d7c8f6a770700
SPL <aprs12km@hotmail.com>
2015-07-13 18:22:08 +0200

    Modelo y base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,30 @@
+var models = require('../models/models.js');
+
 // EXPORTA 2 MWs (question y answer)
 // Serán llamados así => router.get('/quizes/question', quizController.question);
 // GET /quizes/question  
 exports.question = function(req, res) {  
-   res.render('quizes/question', {		// Renderiza (render)	=> quizes/question.ejs
-	   pregunta: 'Capital de Italia'}); // pasando un PARAM (pregunta)   
+//   res.render('quizes/question', {		// Renderiza (render)	=> quizes/question.ejs
+//	   pregunta: 'Capital de Italia'}); // pasando un PARAM (pregunta)   
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', { pregunta: quiz[0].pregunta});
+	})
 };  
  
 // GET /quizes/answer  
 exports.answer = function(req, res) {  
+/*
    if (req.query.respuesta === 'Roma'){  
       res.render('quizes/answer', {respuesta: 'Correcto'});  
    } else {  
       res.render('quizes/answer', {respuesta: 'Incorrecto'});  
    }  
+ */
+	models.Quiz.findAll().success(function(quiz) {
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto' });
+		} else {
+			res.render('quizes/answer', { respuesta: 'Incorrecto'});
+		}
+	}) 
 };  
---
+++ models/models.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite:
+var sequelize = new Sequelize(null, null, null, 
+                       {dialect: "sqlite", storage: "quiz.sqlite"}
+                    );
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+/*
+PARA QUE ME FUNCIONASE (COMMIT 9) HE TENIDO QUE SUSTITUIR
+LOS MÉTODOS "success" POR "then" (daba error de compilación: "undefined" object)
+*/
+sequelize.sync().then(function() {
+  // success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',
+      	            respuesta: 'Roma'
+      	         })
+      .then(function(){console.log('Base de datos inicializada')});
+    };
+  });
+});
