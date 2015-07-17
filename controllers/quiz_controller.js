b1adc92435410ff1ca522c38302e3bdee435d3d0
SPL <aprs12km@hotmail.com>
2015-07-17 13:02:19 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,33 @@
 //IMPORT de "models" para poder acceder a BBDD
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
+
 // EXPORTA 2 MWs (question y answer)
 // Serán llamados así => router.get('/quizes/question', quizController.question);
 // GET /quizes/question  
 
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', { quizes: quizes});
-  })
+//    res.render('quizes/index.ejs', { quizes: quizes});
+	res.render('quizes/index', { quizes: quizes});
+  }).catch(function(error) { next(error);})
 };
  
 // GET /quizes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: quiz});
-  })
+//  models.Quiz.find(req.params.quizId).then(function(quiz) {res.render('quizes/show', { quiz: quiz});  })
+	res.render('quizes/show', { quiz: req.quiz});
 }; 
 
 // GET /quizes/:id/answer  
@@ -29,6 +41,7 @@ exports.answer = function(req, res) {
  */
 
 //	models.Quiz.findAll().success(function(quiz) {
+/*
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
 		if (req.query.respuesta === quiz.respuesta) {
 			res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto' });
@@ -36,4 +49,10 @@ exports.answer = function(req, res) {
 			res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto'});
 		}
 	}) 
+*/
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };  
